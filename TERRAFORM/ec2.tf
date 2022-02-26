f73982b19d33b63e83b7c5a09ae5231eefd47ee2
Akil Johnson <dermaz@yahoo.com>
2022-02-26 05:52:12 -0500

    Akil - add test EC2 resource configuration

---
+++ TERRAFORM/ec2.tf
@@ -0,0 +1,9 @@
+#ami needs to match the instance type
+# resource "aws_instance" "axkec2" {
+#   ami           = "ami-0f7691f59fd7c47af"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     name = " AXK EC2 Test"
+#   }
+# }
