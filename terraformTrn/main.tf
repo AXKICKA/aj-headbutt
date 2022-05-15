85f5f96b9511e197c876ba6291b5a49949f2f505
AXKICKA <94814093+AXKICKA@users.noreply.github.com>
2022-05-14 20:39:07 -0400

    Update main.tf notes (#17)
    * Revert "Akil - Update README"
    
    This reverts commit ecea52c3e651fcacc052a18cb9713d9702206bdd.
    
    * Revert "Revert "Akil - Update README""
    
    This reverts commit b617294a90fbb5e93067ade627d8ec695792b7be.
    
    see if this will undo a revert
    
    * Akil - Update README to test alias
    
    * Akil - add some notes to main.tf
    
    * Akil - correct spelling in main.tf

--- terraformTrn/main.tf
+++ terraformTrn/main.tf
@@ -3,25 +3,25 @@ provider "aws" {
   # profile = "username"
 }
 
-
+# Used to configure a terraform local state
 # terraform {
 #   backend "local" {
 #     path = "./terraform.tfstate"
 #   }
 # }
 
-
+# Used to configure a terraform workspace
 # terraform {
 #   backend "local" {
 #     workspace_dir = "/path/to/terraform.tfstate.d"
 #   }
 # }
 
-
+# Used to configure a terraform remote state 
 terraform {
   backend "s3" {
-    bucket = "axkterraformst"
-    key    = "terrform.tfstate"
+    bucket = "axkterraformst"    #This is the S3 bucket name created in AWS
+    key    = "terrform.tfstate"  #This is the name of the tfstate file within the bucket
     region = "us-east-1"
   }
 }
